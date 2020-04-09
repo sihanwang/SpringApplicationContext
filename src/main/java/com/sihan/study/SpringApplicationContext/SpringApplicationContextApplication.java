274fd16eb655957aaaa98fe4bffcd3a1c2026476
sihanwang <ocpwj@hotmail.com>
2020-04-09 09:14:58 +0800

    init

---
+++ src/main/java/com/sihan/study/SpringApplicationContext/SpringApplicationContextApplication.java
@@ -0,0 +1,13 @@
+package com.sihan.study.SpringApplicationContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplicationContextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sihan/study/SpringApplicationContext/SpringApplicationContextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihan.study.SpringApplicationContext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
